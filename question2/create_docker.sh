4ebc3cefec8de16b1e9d88d7b4febb6bacb5be80
Abdelatif Ghatasheh <32426948+aceabd@users.noreply.github.com>
2022-06-04 20:49:08 +0300

    Update create_docker.sh

--- question2/create_docker.sh
+++ question2/create_docker.sh
@@ -1,15 +1,17 @@
-# Create docker network so the docker container would be able to connect with
-#  each other by name instead of ip
-docker network create network
+docker stop "$(docker ps -q)" > /dev/null 2>&1
 
-# Add running docker containers to the network
-docker network connect network airflow_airflow-scheduler_1
-docker network connect network airflow_airflow-webserver_1
-docker network connect network airflow_airflow-worker_1
-docker network connect network airflow_airflow-scheduler_1
-docker network connect network airflow_flower_1
-docker network connect network airflow_postgres_1
-docker network connect network airflow_redis_1
+mkdir -p logs plugins postgres
+docker-compose up -d airflow-init
 
-docker network connect network dev-postgres
-docker network connect network mongo-db
+docker-compose up -d
+
+sleep 5
+
+docker cp ./script.sql de_postgres:/file.sql
+docker cp ./small.csv de_postgres:/file.csv
+docker exec -u postgres de_postgres psql postgres postgres -f /file.sql > /dev/null 2>&1
+docker exec airflow mkdir -p /home/airflow/data
+
+sleep 10
+
+curl -X PATCH "http://localhost:8080/api/v1/dags/data_transformation" -H  "accept: application/json" -H  "Content-Type: application/json" -H "Authorization: Basic YWlyZmxvdzphaXJmbG93" -d "{\"is_paused\":false}" > /dev/null 2>&1
